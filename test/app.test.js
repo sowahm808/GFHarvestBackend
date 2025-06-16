878f065d72288296e7fe23c7c82eb38e36878766
sowahm808 <sowahm808@gmail.com>
2025-06-16 01:28:34 -0500

    Add mental status log endpoints

--- test/app.test.js
+++ test/app.test.js
@@ -7,3 +7,12 @@ describe('API availability', () => {
     expect(res.statusCode).toEqual(200);
   });
 });
+
+describe('Auth middleware', () => {
+  it('should reject unauthorized mental status POST', async () => {
+    const res = await request(app)
+      .post('/api/mental-status')
+      .send({ childId: 'c1', status: 'ok' });
+    expect(res.statusCode).toEqual(401);
+  });
+});
